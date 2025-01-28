0132a7f072f516a8128c57049cc5243bc7a67730
Athmica <156082578+Athmica@users.noreply.github.com>
2025-01-28 22:13:03 +0530

    Update browse-locustfile.py

--- browse-locustfile.py
+++ browse-locustfile.py
@@ -2,31 +2,35 @@ from locust import task, run_single_user
 from locust import FastHttpUser
 
 
-class BrowseUser(FastHttpUser):
+class browse(FastHttpUser):
     host = "http://localhost:5000"
     default_headers = {
         "Accept-Encoding": "gzip, deflate, br, zstd",
         "Accept-Language": "en-US,en;q=0.5",
         "Connection": "keep-alive",
         "DNT": "1",
+        "Sec-GPC": "1",
         "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:128.0) Gecko/20100101 Firefox/128.0",
     }
 
     @task
-    def browse(self):
-        response = self.client.get(
+    def t(self):
+        with self.client.request(
+            "GET",
             "/browse",
             headers={
-                "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+                "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/png,image/svg+xml,*/*;q=0.8",
+                "Host": "localhost:5000",
+                "Priority": "u=0, i",
+                "Sec-Fetch-Dest": "document",
+                "Sec-Fetch-Mode": "navigate",
+                "Sec-Fetch-Site": "cross-site",
                 "Upgrade-Insecure-Requests": "1",
             },
             catch_response=True,
-        )
-        # Validate response
-        if response.status_code == 200 and "<html>" in response.text:
-            response.success()
-        else:
-            response.failure(f"Unexpected response: {response.status_code}")
+        ) as resp:
+            pass
+       
 
 if __name__ == "__main__":
-    run_single_user(BrowseUser)
+    run_single_user(browse)
